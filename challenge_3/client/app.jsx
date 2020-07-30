a46f0c10337e2b1b139844f5bbad6eb4653b8bc8
Alex Wu <alexcellence@gmail.com>
2020-07-29 22:50:52 -0700

    Create ability to change to F1 on button click

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,183 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      homepage: true,
+      F1: false,
+      F2: false,
+      F3: false,
+      confirmation: false
+    }
 
+    this.handleClick = this.handleClick.bind(this);
+    this.changePage = this.changePage.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    console.log(event.target.value);
+    this.changePage(event.target.value);
+  }
+
+  componentDidMount() {
+    this.setState({
+      homepage: true,
+      F1: false,
+      F2: false,
+      F3: false,
+      confirmation: false
+    });
+  }
+
+  changePage(toggleKey) {
+    var stateCopy = {...this.state};
+    var nextValue = !stateCopy[toggleKey];
+    Object.keys(stateCopy).forEach(key => stateCopy[key] = false);
+    stateCopy[toggleKey] = nextValue;
+    this.setState(stateCopy);
+  }
+
+  render() {
+    // create variables and if statements that will show different forms
+    // var page;
+    // if (this.state = {
+    //   homepage: true,
+    //   F1: false,
+    //   F2: false,
+    //   F3: false,
+    //   confirmation: false
+    // }) {
+    //   page = <button onClick={this.handleClick}>Checkout</button>
+    // } else if (
+    //   this.state = {
+    //     homepage: false,
+    //     F1: true,
+    //     F2: false,
+    //     F3: false,
+    //     confirmation: false
+    //   }
+    // ) {
+    //   page = <FormOne />
+    // }
+
+    return (
+      <div>
+        {this.state.homepage === true ? <HomePage handleClick={this.handleClick}/> : null}
+        {this.state.F1 === true ? <FormOne /> : null}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+function HomePage(props) {
+  return(
+    <div>
+      <button value={'F1'} onClick={props.handleClick}>Checkout</button>
+    </div>
+  )
+}
+
+class FormOne extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event);
+    this.setState({
+      name: '',
+      email: '',
+      password: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label>Name</label>
+            <input type='text' value={this.state.name} onChange={this.handleChange}/>
+        </form>
+        <form>
+          <label>Email</label>
+            <input type='text' value={this.state.email} onChange={this.handleChange}/>
+        </form>
+        <form>
+          <label>Password</label>
+            <input type='text' value={this.state.password} onChange={this.handleChange}/>
+        </form>
+        <button>Next</button>
+      </div>
+    )
+  }
+}
+
+class FormTwo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      addressOne: '',
+      addressTwo: '',
+      city: '',
+      state: '',
+      zipCode: '',
+      phoneNumber: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    console.log(event);
+    this.setState({
+      addressOne: '',
+      addressTwo: '',
+      city: '',
+      state: '',
+      zipCode: '',
+      phoneNumber: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label>Address</label>
+            <input type='text' value={this.state.addressOne} onChange={this.handleChange}/>
+        </form>
+        <form>
+          <label>Address</label>
+            <input type='text' value={this.state.addressTwo} onChange={this.handleChange}/>
+        </form>
+        <form>
+          <label>City</label>
+            <input type='text' value={this.state.city} onChange={this.handleChange}/>
+        </form>
+        <form>
+          <label>State</label>
+            <input type='text' value={this.state.state} onChange={this.handleChange}/>
+        </form>
+        <form>
+          <label>Zip Code</label>
+            <input type='text' value={this.state.zipCode} onChange={this.handleChange}/>
+        </form>
+        <form>
+          <label>Phone Number</label>
+            <input type='text' value={this.state.phoneNumber} onChange={this.handleChange}/>
+        </form>
+        <button>Next</button>
+      </div>
+    )
+  }
+}
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,22 @@
+// import dependencies
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = 3002;
+
+const dirName = path.join(__dirname, '/public');
+console.log('dirName ', dirName);
 
 
+// middleware
+app.use(express.static(path.join(__dirname, '/public')));
+
+// routes
+
+
+
+// set up server to listen
+app.listen(PORT, () => {
+  console.log(`Server is listening at ${PORT}`);
+})
