c412072a9a8c1930b35aed416803de6e612f698c
Alex Wu <alexcellence@gmail.com>
2020-07-30 22:04:33 -0700

    Refactor FormTwo to be a functional component

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -8,16 +8,12 @@ class App extends React.Component {
       name: '',
       email: '',
       password: '',
-      addressOne: '',
-      addressTwo: '',
+      address1: '',
+      address2: '',
       city: '',
       state: '',
       zipCode: '',
       phoneNumber: '',
-      creditCard: '',
-      expiration: '',
-      cvv: '',
-      billingZipCode: '',
       creditCard: '',
       expiration: '',
       cvv: '',
@@ -50,9 +46,7 @@ class App extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    this.setState({
-      [event.target.name]: event.target.value
-    })
+
   }
 
   handleChange(event) {
@@ -65,10 +59,10 @@ class App extends React.Component {
 
     return (
       <div>
-        {this.state.currentPage === 'homepage' ? <HomePage addData={this.addData} handleClick={this.handleClick}/> : null}
-        {this.state.currentPage === 'F1' ? <FormOne state={this.state}handleChange={this.handleChange} handleClick={this.handleClick}/> : null}
-        {this.state.currentPage === 'F2' ? <FormTwo handleChange={this.handleChange} handleClick={this.handleClick}/> : null}
-        {this.state.currentPage === 'F3' ? <FormThree handleChange={this.handleChange} handleClick={this.handleClick}/> : null}
+        {this.state.currentPage === 'homepage' ? <HomePage state={this.state} addData={this.addData} handleClick={this.handleClick}/> : null}
+        {this.state.currentPage === 'F1' ? <FormOne state={this.state} handleChange={this.handleChange} handleClick={this.handleClick}/> : null}
+        {this.state.currentPage === 'F2' ? <FormTwo state={this.state} handleChange={this.handleChange} handleClick={this.handleClick}/> : null}
+        {this.state.currentPage === 'F3' ? <FormThree state={this.state} handleChange={this.handleChange} handleClick={this.handleClick}/> : null}
       </div>
     )
   }
@@ -104,107 +98,36 @@ function FormOne(props) {
   )
 }
 
-// class FormOne extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       name: '',
-//       email: '',
-//       password: ''
-//     }
-//     this.handleChange = this.handleChange.bind(this);
-//   }
-
-//   handleChange(event) {
-//     console.log(event);
-//     this.setState({
-//       name: '',
-//       email: '',
-//       password: ''
-//     })
-//     this.props.addData(this.state);
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <form>
-//           <label>Name</label>
-//             <input type='text' value={this.state.name} onChange={this.handleChange}/>
-//         </form>
-//         <form>
-//           <label>Email</label>
-//             <input type='text' value={this.state.email} onChange={this.handleChange}/>
-//         </form>
-//         <form>
-//           <label>Password</label>
-//             <input type='text' value={this.state.password} onChange={this.handleChange}/>
-//         </form>
-//         <button value={'F2'} onClick={this.props.handleClick}>Next</button>
-//       </div>
-//     )
-//   }
-// }
-
-class FormTwo extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      addressOne: '',
-      addressTwo: '',
-      city: '',
-      state: '',
-      zipCode: '',
-      phoneNumber: ''
-    }
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    console.log(event);
-    this.setState({
-      addressOne: '',
-      addressTwo: '',
-      city: '',
-      state: '',
-      zipCode: '',
-      phoneNumber: ''
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <form>
-          <label>Address</label>
-            <input type='text' value={this.state.addressOne} onChange={this.handleChange}/>
-        </form>
-        <form>
-          <label>Address</label>
-            <input type='text' value={this.state.addressTwo} onChange={this.handleChange}/>
-        </form>
-        <form>
-          <label>City</label>
-            <input type='text' value={this.state.city} onChange={this.handleChange}/>
-        </form>
-        <form>
-          <label>State</label>
-            <input type='text' value={this.state.state} onChange={this.handleChange}/>
-        </form>
-        <form>
-          <label>Zip Code</label>
-            <input type='text' value={this.state.zipCode} onChange={this.handleChange}/>
-        </form>
-        <form>
-          <label>Phone Number</label>
-            <input type='text' value={this.state.phoneNumber} onChange={this.handleChange}/>
-        </form>
-        <button value={'F3'} onClick={this.props.handleClick}>Next</button>
-      </div>
-    )
-  }
+function FormTwo(props) {
+  return (
+    <div>
+      <form>
+        <label>Address</label>
+          <input type='text' name='address1' value={props.state.addressOne} onChange={props.handleChange}/>
+      </form>
+      <form>
+        <label>Address</label>
+          <input type='text' name='address2' value={props.state.addressTwo} onChange={props.handleChange}/>
+      </form>
+      <form>
+        <label>City</label>
+          <input type='text' name='city' value={props.state.city} onChange={props.handleChange}/>
+      </form>
+      <form>
+        <label>State</label>
+          <input type='text' name='state' value={props.state.state} onChange={props.handleChange}/>
+      </form>
+      <form>
+        <label>Zip Code</label>
+          <input type='text' name='zipCode' value={props.state.zipCode} onChange={props.handleChange}/>
+      </form>
+      <form>
+        <label>Phone Number</label>
+          <input type='text' name='phoneNumber' value={props.state.phoneNumber} onChange={props.handleChange}/>
+      </form>
+      <button value={'F3'} onClick={props.handleClick}>Next</button>
+    </div>
+  )
 }
 
 class FormThree extends React.Component {
